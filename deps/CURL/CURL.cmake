c7e3581d8b12e9099cc96ef73da3986da3d30139
Bastien Nocera <hadess@hadess.net>
2024-01-02 13:24:55 +0100

    deps: Use openssl from system
    It's only used by cURL, and cURL is already used from the system if
    available.

--- deps/CURL/CURL.cmake
+++ deps/CURL/CURL.cmake
@@ -72,10 +72,6 @@ bambustudio_add_cmake_project(CURL
     ${_curl_platform_flags}
 )
 
-if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-  add_dependencies(dep_CURL dep_OpenSSL)
-endif ()
-
 if (MSVC)
     add_debug_dep(dep_CURL)
 endif ()
